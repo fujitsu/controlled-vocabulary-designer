5bad47886e2c1259cbe9d09fd07d7dbb81bac8e9
m-yasutaka <m.yasutaka@fujitsu.com>
2021-06-23 14:39:30 +0900

    Update Hensyugoi.sh

--- example-inputdata-creation/mountdir/src/Hensyugoi.sh
+++ example-inputdata-creation/mountdir/src/Hensyugoi.sh
@@ -7,7 +7,7 @@ echo
 
 PHASE="WordSeparation"
 echo ${PHASE}
-python3 ./${PHASE}.py -c config.json -i wiki.txt -o wiki_wakati_preprocessed.txt WordSeparation.npy || TEST_OK=NG
+python3 ./${PHASE}.py -c config.json -i ../data/wiki.txt -o ../data/wiki_wakati_preprocessed.txt WordSeparation.npy || TEST_OK=NG
 if [ "${TEST_OK}" != "OK" ]; then
     echo "${PHASE} Failed"
     exit 1
@@ -15,7 +15,7 @@ fi
 
 PHASE="WordEmbedding"
 echo ${PHASE}
-python3 ./${PHASE}.py -c config.json -i wiki_wakati_preprocessed.txt tag.csv domain_wakati_preprocessed.txt -o WordEmbedding.npy WordEmbedding.model || TEST_OK=NG
+python3 ./${PHASE}.py -c config.json -i ../data/wiki_wakati_preprocessed.txt ../data/tag.csv ../data/domain_wakati_preprocessed.txt -o WordEmbedding.npy WordEmbedding.model || TEST_OK=NG
 if [ "${TEST_OK}" != "OK" ]; then
     echo "${PHASE} Failed"
     exit 1
@@ -23,7 +23,7 @@ fi
 
 PHASE="SynonymExtraction"
 echo ${PHASE}
-python3 ./${PHASE}.py -c config.json -i tag.csv domain_wakati_preprocessed.txt WordEmbedding.model -o SynonymExtraction.npy || TEST_OK=NG
+python3 ./${PHASE}.py -c config.json -i ../data/tag.csv ../data/domain_wakati_preprocessed.txt WordEmbedding.model -o SynonymExtraction.npy || TEST_OK=NG
 if [ "${TEST_OK}" != "OK" ]; then
     echo "${PHASE} Failed"
     exit 1
@@ -31,7 +31,7 @@ fi
 
 PHASE="HypernymExtraction"
 echo ${PHASE}
-python3 ./${PHASE}.py -c config.json -i wiki_wakati_preprocessed.txt tag.csv -o HypernymExtraction.npy || TEST_OK=NG
+python3 ./${PHASE}.py -c config.json -i ../data/wiki_wakati_preprocessed.txt ../data/tag.csv -o HypernymExtraction.npy || TEST_OK=NG
 if [ "${TEST_OK}" != "OK" ]; then
     echo "${PHASE} Failed"
     exit 1
@@ -39,7 +39,7 @@ fi
 
 PHASE="Filtering"
 echo ${PHASE}
-python3 ./${PHASE}.py -c config.json -i tag.csv domain_wakati_preprocessed.txt WordEmbedding.npy -o Filtering.npy || TEST_OK=NG
+python3 ./${PHASE}.py -c config.json -i ../data/tag.csv ../data/domain_wakati_preprocessed.txt WordEmbedding.npy -o Filtering.npy || TEST_OK=NG
 if [ "${TEST_OK}" != "OK" ]; then
     echo "${PHASE} Failed"
     exit 1
@@ -47,7 +47,7 @@ fi
 
 PHASE="Hensyugoi"
 echo ${PHASE}
-python3 ./${PHASE}.py -c config.json -i WordSeparation.npy WordEmbedding.npy SynonymExtraction.npy HypernymExtraction.npy Filtering.npy -o Hensyugoi.csv || TEST_OK=NG
+python3 ./${PHASE}.py -c config.json -i WordSeparation.npy WordEmbedding.npy SynonymExtraction.npy HypernymExtraction.npy Filtering.npy -o ../data/Hensyugoi.csv || TEST_OK=NG
 if [ "${TEST_OK}" != "OK" ]; then
     echo "${PHASE} Failed"
     exit 1
