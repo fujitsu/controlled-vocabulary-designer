a36e744c12787b25440f84058ed89dffa3a828d9
fj-mtachi <fj7038im@aa.jp.fujitsu.com>
2022-03-18 11:03:35 +0900

    Update TextFieldOfBroaderTerm.js, TextFieldOfPreferredLabel.js, TextFieldOfSynonym.js, EditingVocabulary.js - Display citation sources side by side without grouping

--- app/app/client/components/TextFieldOfSynonym.js
+++ app/app/client/components/TextFieldOfSynonym.js
@@ -123,24 +123,32 @@ export default
                 renderOption={(option, {selected}) => (
                   <React.Fragment>
                     <div style={{width: '100%'}}>
-                      <Box
-                        component="span"
-                        display="block"
-                        style={{fontSize: '16px'}}
-                      >
-                        {option}
-                      </Box>
-                      <Box
-                        component="span"
-                        display="block"
-                        style={{fontSize: '10px'}}
-                      >
-                        {
-                          this.props.editingVocabulary.getReferenceFromData(
-                              option,
-                              'Synonym',
-                          )
-                        }
+                      <Box display="flex" flexDirection="row" alignItems="center">
+                        <Box
+                          component="span"
+                          display="inline"
+                          style={{fontSize: '16px',whiteSpace: 'nowrap'}}
+                        >
+                          {option}
+                        </Box>
+                        <Box
+                          component="span"
+                          display="inline"
+                          title={
+                            this.props.editingVocabulary.getReferenceFromData(
+                                option,
+                                'Synonym',
+                            )
+                          }
+                          style={{fontSize: '10px',whiteSpace: 'nowrap',textOverflow: 'ellipsis', overflowX: 'hidden', marginLeft: '10px'}}
+                        >
+                          {
+                            this.props.editingVocabulary.getReferenceFromData(
+                                option,
+                                'Synonym',
+                            )
+                          }
+                        </Box>
                       </Box>
                     </div>
                   </React.Fragment>
