408e10ec7635649074c9163b91630c18062b2d36
fj-mtachi <fj7038im@aa.jp.fujitsu.com>
2022-06-17 11:17:59 +0900

    Update EditPanelVocabularyTab.js, TextFieldOfUri.js - Change uri input item to non-editable

--- app/app/client/components/TextFieldOfUri.js
+++ app/app/client/components/TextFieldOfUri.js
@@ -7,12 +7,7 @@ import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import CreateIcon from '@material-ui/icons/Create';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import Snackbar from '@material-ui/core/Snackbar';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
 import config from '../config/Config';
 
 
@@ -34,128 +29,10 @@ export default
     super(props);
     this.state = {
       disabledFlg: true,
-      modeChange: true,
-      editTerm: '',
-      open: false,
-      message: '',
       config: config,
     };
   }
 
-  /**
-   * componentDidUpdate
-   */
-   componentDidUpdate(prevProps, prevState) {
-    
-    if (prevProps.disabledFlg !== this.props.disabledFlg) {
-      if ( !this.props.disabledFlg) {
-        this.setState({
-          disabledFlg: false,
-          editTerm: this.props.editingVocabulary.currentNode.term,
-        });
-      } else {
-        this.setState({disabledFlg: true});
-      }
-    }else{
-      return;
-    }
-  }
-
-  /**
-   * Warning displaying snackbar events
-   * @param {String} errorMsg - error message
-   */
-  openSnackbar(errorMsg) {
-    this.setState({open: true, message: errorMsg});
-  }
-
-  /**
-   * Warning hiding snackbar event
-   */
-  handleClose() {
-    this.setState({open: false, message: ''});
-  };
-
-  /**
-   * Delete event
-   * @param  {object} event - information of event
-   */
-  onDelete(event) {
-    this.props.editingVocabulary.updataUri('');
-  }
-
-  /**
-   * Key press event
-   * @param  {object} event - information of event
-   */
-  handleKeyDown(event) {
-    if (event.keyCode == 13 || event.keyCode == 32) {
-      this.updataUri(event);
-    }
-    event.stopPropagation();
-  }
-
-  /**
-   * URI edit event
-   * @param  {object} event - information of event
-   */
-  uriTextEdit(event) {
-    const currentUri = this.props.editingVocabulary.currentUri;
-    event.target.value = currentUri;
-  }
-
-  /**
-   * URI input event
-   * @param  {object} event - information of event
-   */
-  uriTextEnter(event) {
-    this.updataUri(event);
-  }
-
-  /**
-   * URI update
-   * @param  {object} event - information of event
-   */
-  updataUri(event) {
-    let uri = event.target.value;
-
-    // Formalize URI prefix
-    const prefixList = this.state.config.prefix;
-    const foundPrefix = prefixList.find((prefix) => {
-      return uri.startsWith(prefix.equiv);
-    });
-    if (foundPrefix) {
-      uri = uri.replace(foundPrefix.equiv, foundPrefix.origin);
-    }
-
-    const prfrrdLbl = this.props.editingVocabulary.tmpPreferredLabel.list[0];
-
-    if (this.props.editingVocabulary.isInvalidUri(uri, prfrrdLbl)) {
-      let errorMsg = '代表語のURIテキストボックスに、¥n' +
-                     '同義関係でない別の代表語 \"' +
-                     this.props.editingVocabulary.equalUriPreferredLabel +
-                     '\" と同じ代表語のURIが記入されています。¥n' +
-                     '代表語のURIテキストボックスには、¥n' +
-                     '既に登録されている他の代表語のURIとは異なる値を記入してください。';
-      errorMsg = errorMsg.split('¥n').map((line, key) =>
-        <span key={key}>{line}<br /></span>);
-      this.setState({modeChange: true});
-      setTimeout(() => {
-        this.openSnackbar(errorMsg);
-      }, 100);
-    } else {
-      this.setState({modeChange: true, disabledFlg: true});
-    }
-    this.props.editingVocabulary.updataUri(uri);
-  }
-
-  /**
-   * URI edit confirm event
-   * @param  {object} event - information of event
-   */
-  onChange(event) {
-    this.setState({modeChange: false});
-  };
 
   /**
    * render
@@ -212,18 +89,9 @@ export default
       alteredUri = finalUri;
     }
 
+    const  disabledFlg = true;
 
-    const currentTerm = this.props.editingVocabulary.currentNode.term;
-    let disabledFlg = this.state.disabledFlg;
-    if (this.state.editTerm !== currentTerm) {
-      disabledFlg = true;
-    }
-    if (this.props.disabled) {
-      disabledFlg = true;
-    }
     const currentUri = this.props.editingVocabulary.currentNode.uri;
-    // console.log('currentUri(currentNode):', currentUri);
-
 
     return (
       <div>
@@ -236,9 +104,7 @@ export default
                   multiple
                   freeSolo
                   disabled={disabledFlg}
-                  style={
-                    this.state.modeChange?{display: 'inline'}:{display: 'none'}
-                  }
+                  style={{display: 'inline'}}
                   value={alteredUri}
                   classes={
                     {
@@ -257,7 +123,6 @@ export default
                         {...getTagProps({index})}
                         label={option}
                         data={currentUri}
-                        onDelete={(event) => this.onDelete(event)}
                         config={this.state.config}
                       />
                     ));
@@ -265,7 +130,6 @@ export default
                   renderInput={(params) => (
                     <TextField
                       {...params}
-                      onFocus={(event)=>this.onChange(event)}
                       variant="standard"
                       style={
                         disabledFlg ?
@@ -275,51 +139,11 @@ export default
                     />
                   )}
                 />
-
-                <div
-                  style={this.state.modeChange ?
-                    {display: 'none'}:
-                    {display: 'inline'}}
-                >
-                  <TextField
-                    classes={{root: this.props.classes.textField}}
-                    onFocus={(event)=>this.uriTextEdit(event)}
-                    onKeyDown={(event)=>this.handleKeyDown(event)}
-                    onBlur={(event)=>this.uriTextEnter(event)}
-                    disabled={this.state.disabledFlg}
-                    inputProps={
-                      {style: {textAlign: 'center', paddingTop: '12px'}}
-                    }
-                    inputRef={(input) => input && input.focus()}
-                  />
-                </div>
               </Box>
             </Grid>
           </Grid>
         </form>
-        <Snackbar
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          open={this.state.open}
-          onClose={() => this.handleClose()}
-          message={this.state.message}
-          action={
-            <React.Fragment>
-              <IconButton
-                size="small"
-                aria-label="close"
-                color="inherit"
-                onClick={() => this.handleClose()}
-              >
-                <CloseIcon fontSize="small" />
-              </IconButton>
-            </React.Fragment>
-          }
-        />
       </div>
-
     );
   }
 }
@@ -327,6 +151,4 @@ export default
 TextFieldOfUri.propTypes = {
   editingVocabulary: PropTypes.object,
   classes: PropTypes.object,
-  disabled: PropTypes.bool,
-  disabledFlg: PropTypes.bool,
 };
